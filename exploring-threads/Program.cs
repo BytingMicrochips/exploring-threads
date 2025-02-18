2745c69312570e77a05283496eae829b4ae7db0d
Windows10 <Windows10@DESKTOP-VMTTBRK>
2025-02-18 12:22:06 +0000

    Add project files.

---
+++ exploring-threads/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace exploringThreads
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Task backgroundTask = new Task(() =>
+            {
+                Console.WriteLine("Background task is starting");
+                bool result = SomethingAsync();
+                Console.WriteLine($"Background task has completed: {result}");
+            });
+
+            Console.WriteLine("Main thread is running");
+            backgroundTask.Start();
+            Thread.Sleep(2000);
+            Console.WriteLine("Main thread is completed");
+            backgroundTask.Wait();
+        }
+
+        static bool SomethingAsync()
+        {
+            Thread.Sleep(3000);
+            return true;
+        }
+    }
+}
+
+// wait all
+// timer
+// kill itself after timer
